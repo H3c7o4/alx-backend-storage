833eeca3344b8af09658ff0807484de21f21e192
H3c7o4 <hectorvladitok@gmail.com>
2023-07-19 11:39:04 +0200

    Add a get method in the Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,3 +26,48 @@ class Cache(object):
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """
+
+        Args:
+           key: string argument
+           fn: Callable argument
+
+        Returns:
+             None
+        """
+        data = self._redis.get(key)
+
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+
+        Args:
+           key: string argument
+
+        Returns:
+             A string
+        """
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """
+
+        Args:
+           key: string argument
+
+        Returns:
+              An integer
+        """
+        data = self._redis.get(key)
+        try:
+            data = int(value.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
