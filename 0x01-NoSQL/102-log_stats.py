2da5dc4b72fc0739546c8789b10838ea211eb574
H3c7o4 <hectorvladitok@gmail.com>
2023-07-20 13:19:48 +0200

    Fix the error in 102-log_stats.py file

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,45 +1,32 @@
 #!/usr/bin/env python3
-""" Log stats - new version """
+""" nginx logs to mongo instance parser """
+
 from pymongo import MongoClient
 
 
-def nginx_stats_check():
-    """ provides some stats about Nginx logs stored in MongoDB:"""
+def temp_F():
+    """ temp function to check the checker"""
     client = MongoClient()
-    collection = client.logs.nginx
-
-    num_of_docs = collection.count_documents({})
-    print("{} logs".format(num_of_docs))
+    main = client.logs.nginx
+    method_C = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    status = main.count_documents({"method": "GET", "path": "/status"})
+    print(f"{main.estimated_document_count()} logs")
     print("Methods:")
-    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods_list:
-        method_count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, method_count))
-    status = collection.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status))
-
+    for i in method_C:
+        check = main.count_documents({"method": i})
+        print(f"\tmethod {i}: {check}")
+    print(f"{status} status check")
+    count = 0
+    aggr = [{"$group": {"_id": '$ip', "c": {"$sum": 1}}}, {"$sort": {"c": -1}}]
+    db = client.logs
+    ip = {x["_id"]: x["c"] for x in db.nginx.aggregate(aggr)}
     print("IPs:")
-
-    top_IPs = collection.aggregate([
-        {"$group":
-         {
-             "_id": "$ip",
-             "count": {"$sum": 1}
-         }
-         },
-        {"$sort": {"count": -1}},
-        {"$limit": 10},
-        {"$project": {
-            "_id": 0,
-            "ip": "$_id",
-            "count": 1
-        }}
-    ])
-    for top_ip in top_IPs:
-        count = top_ip.get("count")
-        ip_address = top_ip.get("ip")
-        print("\t{}: {}".format(ip_address, count))
+    for key, value in ip.items():
+        if count > 9:
+            break
+        count += 1
+        print(f"\t{key}: {value}")
 
 
 if __name__ == "__main__":
-    nginx_stats_check()
+    temp_F()
