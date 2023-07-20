4437befe87e3a465376965c80b3aaf32a55767b3
H3c7o4 <hectorvladitok@gmail.com>
2023-07-20 13:25:17 +0200

    Fix the error in 102-log_stats.py file

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
-""" nginx logs to mongo instance parser """
-
+"""Print info in a collection"""
 from pymongo import MongoClient
 
-
-def temp_F():
-    """ temp function to check the checker"""
-    client = MongoClient()
-    main = client.logs.nginx
-    method_C = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    status = main.count_documents({"method": "GET", "path": "/status"})
-    print(f"{main.estimated_document_count()} logs")
-    print("Methods:")
-    for i in method_C:
-        check = main.count_documents({"method": i})
-        print(f"\tmethod {i}: {check}")
-    print(f"{status} status check")
-    count = 0
-    aggr = [{"$group": {"_id": '$ip', "c": {"$sum": 1}}}, {"$sort": {"c": -1}}]
-    db = client.logs
-    ip = {x["_id"]: x["c"] for x in db.nginx.aggregate(aggr)}
-    print("IPs:")
-    for key, value in ip.items():
-        if count > 9:
-            break
-        count += 1
-        print(f"\t{key}: {value}")
-
-
 if __name__ == "__main__":
-    temp_F()
+    """ Make a check for all elements in a collention """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print(f"{collection.estimated_document_count()} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
+
+    check_get = collection.count_documents({
+        'method': 'GET', 'path': "/status"
+    })
+    print(f"{check_get} status check")
+
+    print("IPs:")
+    top_ips = collection.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+    for ip in top_ips:
+        print(f"\t{ip.get('ip')}: {ip.get('count')}")
