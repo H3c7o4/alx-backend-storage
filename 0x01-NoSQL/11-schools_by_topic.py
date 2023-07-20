907b2859e09f3821da1493631c9493f58bba5456
H3c7o4 <hectorvladitok@gmail.com>
2023-07-20 13:17:38 +0200

    Fix the error in 11-schools_by_topic.py file

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -15,4 +15,4 @@ def schools_by_topic(mongo_collection, topic):
     Returns:
          the list of school having a specific topic
     """
-    return mongo_collection.find({"topic": {"$in": [topic]}})
+    return mongo_collection.find({"topics": topic})
