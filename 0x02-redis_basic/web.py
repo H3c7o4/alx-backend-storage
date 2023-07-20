7306f9feab99d4bbfb0ca94d62478fcc9acb7683
H3c7o4 <hectorvladitok@gmail.com>
2023-07-20 13:31:24 +0200

    Fix bugs in web.py file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-"""
-This module contains the function get_page(url)
-"""
-import redis
+""" Redis with requests """
 import requests
-r = redis.Redis()
-count = 0
+from functools import wraps
+from typing import Callable
+import redis
 
 
+def count(method: Callable):
+    """ Count the call to requests """
+    r = redis.Redis()
+
+    @wraps(method)
+    def wrapped(url):
+        """ function that will count """
+        r.incr(f"count:{url}")
+        expiration_count = r.get(f"cached:{url}")
+        if expiration_count:
+            return expiration_count.decode('utf-8')
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapped
+
+
+@count
 def get_page(url: str) -> str:
-    """
-
-    Args:
-       url: link to the page
-
-    Returns:
-         HTML content
-    """
-    r.set(f'cached:{url}', count)
-    resp = requests.get(url)
-    r.incr(f'count:{url}')
-    r.setex(f'cached:{url}', 10, r.get(f'cached:{url}'))
-    return resp.text
-
-
-if __name__ == "__main__":
-    data = get_page('http://slowwly.robertomurray.co.uk')
-    print(data)
+    """ module to obtain the HTML """
+    return requests.get(url).text
